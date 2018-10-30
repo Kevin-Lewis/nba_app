569bde66d7e221c14f8ed03e9d32324945e0426e
Kevin Lewis <kevlew02@gmail.com>
2018-10-30 00:31:07 -0400

    initial commit

---
+++ nba_project/nba_app/tests.py
@@ -0,0 +1,10 @@
+from django.urls import reverse
+from django.urls import resolve
+from django.test import TestCase
+
+class HomeTests(TestCase):
+	def test_home_view_status_code(self):
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+
